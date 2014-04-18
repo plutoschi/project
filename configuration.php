3b018d50e75917fcba00aa84435ad863ca98de91
plutoschi <edoardolandi@verdedj.com>
2014-04-18 10:25:52 +0200

    aggiornamento limit

--- configuration.php
+++ configuration.php
@@ -7,7 +7,7 @@ class JConfig {
 	public $sitename = 'prova';
 	public $editor = 'tinymce';
 	public $captcha = '0';
-	public $list_limit = '30';
+	public $list_limit = '50';
 	public $access = '1';
 	public $debug = '0';
 	public $debug_lang = '0';
