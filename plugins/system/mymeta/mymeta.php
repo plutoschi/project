d4e6aa108fda7375661078feabddbb4de6319a28
plutoschi <edoardolandi@verdedj.com>
2014-04-18 10:04:37 +0200

    Primo commit

---
+++ plugins/system/mymeta/mymeta.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2005 - 2011 Mark Dexter and Louis Landry. 
+ * @license	GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// no direct access
+defined('_JEXEC') or die;
+jimport('joomla.plugin.plugin');
+/**
+ * Example System Plugin
+ */
+class plgSystemMyMeta extends JPlugin
+{
+     function onBeforeCompileHead()
+     {
+        if ($this->params->get('revised')) {
+           $document = JFactory::getDocument();
+           $headData = $document->getHeadData();
+           $headData['metaTags']['standard']['revised'] =  
+              $this->params->get('revised');
+           $document->setHeadData($headData);             }
+           
+        if ($this->params->get('change title')) {
+           $document = JFactory::getDocument();
+           $headData = $document->getHeadData();
+           $headData['metaTags']['standard']['change title'] =  
+              $this->params->get('change title');
+           $document->setHeadData($headData);             }   
+     }
+}
